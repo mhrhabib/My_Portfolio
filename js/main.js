51b144d51269d8b744256a75e9d660e163ba211d
Habib <mhrhabib39@gmail.com>
2020-02-18 16:49:11 +0600

    about section added

--- js/main.js
+++ js/main.js
@@ -4,4 +4,13 @@ $(document).ready(function(){
         play: 5000,
         pagination:false
     });
+    
+    var typed = new Typed(".typed",{
+        strings: [ "Front End Developer.", "web developer","student"],
+        typeSpeed: 100,
+        loop: true,
+        startDelay: 1000,
+        showCursor: false
+    });
+    
 });
